75f7dd3c998ffa90bb4069a96c8594a5b372b5d8
Samuel B. Oldra <samuel.oldra@gmail.com>
2015-06-01 19:00:00 -0300

    Primeiro commit

---
+++ Designer/Form1.cs
@@ -0,0 +1,10 @@
+﻿namespace Designer
+{
+    public partial class Form1 : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
