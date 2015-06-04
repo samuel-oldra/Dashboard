a3a79e9a6ac5138a79ad122969c5bf9c4f8c3535
Samuel B. Oldra <samuel.oldra@gmail.com>
2015-06-04 19:00:00 -0300

    Reformatado e reorganizado código

--- Designer/Program.cs
+++ Designer/Program.cs
@@ -5,6 +5,8 @@ namespace Designer
 {
     internal static class Program
     {
+        #region Private Methods
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -15,5 +17,7 @@ namespace Designer
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
+
+        #endregion Private Methods
     }
 }
--- Viewer/Form1.cs
+++ Viewer/Form1.cs
@@ -2,10 +2,14 @@
 {
     public partial class Form1 : DevExpress.XtraEditors.XtraForm
     {
+        #region Public Constructors
+
         public Form1()
         {
             InitializeComponent();
             dashboardViewer1.DashboardSource = new System.Uri("Dashboard.xml", System.UriKind.Relative);
         }
+
+        #endregion Public Constructors
     }
 }
